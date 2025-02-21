97bdedf2c40fdca788b7e67846285e35920b93a6
Willy <willy548798@gmail.com>
2025-02-21 17:10:21 +0800

    make video player

--- MyVedioPlayer/ViewController.swift
+++ MyVedioPlayer/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var videoView: UIView!
+    var player: AVPlayer?
+    var playLayer: AVPlayerLayer?
+    let videoURLString = "http://vjs.zencdn.net/v/oceans.mp4"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let url = URL(string: videoURLString){
+            print("url ok")
+            player = AVPlayer(url: url)
+            if let videoPlayer = player{
+                playLayer = AVPlayerLayer(player: videoPlayer)
+                playLayer?.frame = videoView.bounds
+                playLayer?.videoGravity = .resizeAspectFill
+                if let playLayer = playLayer{
+                    videoView.layer.addSublayer(playLayer)
+                }
+                videoPlayer.play()
+            }
+        }
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // 如果需要確保 playerLayer 在 viewDidLayoutSubviews 中正確顯示
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playLayer?.frame = videoView.bounds
+    }
+    
 }
 

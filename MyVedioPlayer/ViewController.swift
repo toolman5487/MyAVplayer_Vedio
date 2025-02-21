fa8ff91d248524a55d0fb24d547ebaf5d20205e7
Willy <willy548798@gmail.com>
2025-02-21 15:16:17 +0800

    Initial Commit

---
+++ MyVedioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyVedioPlayer
+//
+//  Created by Willy Hsu on 2025/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
